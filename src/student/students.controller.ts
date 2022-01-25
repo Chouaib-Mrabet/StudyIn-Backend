1c41fece0913d48ed209eb48bdb19edb8539f4b3
medazizammari <ammarimedaziz@gmail.com>
2022-01-25 09:24:44 +0000

    added dtos and updated service and controller of student

--- src/student/students.controller.ts
+++ src/student/students.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { StudentEntity } from './entities/student.entity';
+import { CreateStudentDto } from './Dto/createStudentDto';
+import { UpdateStudentDto } from './Dto/updateStudentDto';
 
 @Controller('student')
 export class StudentsController {
     constructor(
+
         private studentsService: StudentsService
     ) {
     }
@@ -14,7 +17,7 @@ export class StudentsController {
     ): Promise<StudentEntity[]> {
         return await this.studentsService.getStudents();
     }
-
+    
     @Get(":id")
     async getStudentById(
         @Param('id', ParseIntPipe) id: number,
@@ -23,26 +26,20 @@ export class StudentsController {
     }
 
     @Post('create')
-    async create(
-        @Body() student: StudentEntity
-    ): Promise<any> {
-        return this.studentsService.createStudent(student);
-    }
-
+    async createStudent
+        (@Body() student: CreateStudentDto): Promise<any> {
+      return this.studentsService.createStudent(student);
+    }  
+    
     @Put(':id/update')
-    async update(
-        @Param('id') id,
-        @Body() student: StudentEntity
-    ): Promise<any> {
-        student.id = Number(id);
-        return this.studentsService.updateStudent(student);
-    }
-
+    async update(@Param('id') id, @Body() student: UpdateStudentDto): Promise<any> {
+        id = Number(id);
+        return this.studentsService.updateStudent(id,student);
+    }  
+    
     @Delete(':id/delete')
-    async delete(
-        @Param('id') id
-    ): Promise<any> {
-        return this.studentsService.deleteStudent(id);
+    async delete(@Param('id') id): Promise<any> {
+      return this.studentsService.deleteStudent(id);
     }
 
 
